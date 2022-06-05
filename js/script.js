e36b0952e86bd4597dfd442976bcd45794272c00
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-05 23:11:46 +0200

    commit finale js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ ScrollReveal ({
 
 });
 
-ScrollReveal().reveal('.container', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('#services', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('#first-cta', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('#first-cta h1', { delay: 300, origin: 'left' });
+ScrollReveal().reveal('#first-cta p', { delay: 300, origin: 'left' });
+ScrollReveal().reveal('.container_testimonianze', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.container_testimonianze h4', { delay: 300, origin: 'top' });
+ScrollReveal().reveal('.container_partnership', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.footer', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('#services', { delay: 500, origin: 'top' }); 
 ScrollReveal().reveal('.program_icons', { delay: 500, origin: 'top' });
-ScrollReveal().reveal('.partnerships_logo', { delay: 500, origin: 'top'});
-ScrollReveal().reveal('.footer', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('.partnerships_logo', { delay: 500, origin: 'top' });
+
