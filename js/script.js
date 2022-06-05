2c33f3fa532a3c8a80a4c63d6bb3a3a02ecf1cfd
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-05 18:40:49 +0200

    commit quattro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 ScrollReveal ({
     reset: true,
     distance: '60px',
@@ -9,3 +8,7 @@ ScrollReveal ({
 });
 
 ScrollReveal().reveal('.container', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('#services', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.program_icons', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.partnerships_logo', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('.footer', { delay: 500, origin: 'top'});
